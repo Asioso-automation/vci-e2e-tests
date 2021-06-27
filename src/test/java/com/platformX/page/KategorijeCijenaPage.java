cdb0f9deb84a56b8040191d3f9a9d5cfb3c276df
nebojsa.milojevic <nebojsa.milojevic@qaalpha.com>
2021-06-27 13:16:29 +0200

    dodavanje i izmjena kategorije cijena

--- src/test/java/com/platformX/page/KategorijeCijenaPage.java
+++ src/test/java/com/platformX/page/KategorijeCijenaPage.java
@@ -5,10 +5,31 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 
 public class KategorijeCijenaPage extends PocetnaStranica {
+	
+	@FindBy(xpath = "//div[1]/div/div/div/div[1]/div/input")
+	private WebElement nazivKategorijeWE;
+	
+	@FindBy(xpath = "//div[2]/button[1]")
+	private WebElement dodajKategorijuWE;
+	
+	@FindBy(xpath = "//td[2]/div/div/div/div[1]/input")
+	private WebElement filterNazivWE;
+	
+	@FindBy(xpath = "//tr[2]/td[2]")
+	private WebElement nazivTabelaWE;
+	
+	@FindBy(xpath = "//td[6]/button")
+	private WebElement burgerBarWE;
+	
+	@FindBy(xpath = "//div[contains(text(), 'Uredi')]")
+	private WebElement urediWE;
 
 	public KategorijeCijenaPage(WebDriver driver) throws FileNotFoundException, IOException {
 		super(driver);
@@ -28,6 +49,43 @@ public class KategorijeCijenaPage extends PocetnaStranica {
 		assertTrue(stranicaBtnWE.getText().trim().equals("KATEGORIJE CIJENA"), "KategorijeCijena: Naziv stranice nije dobar!");
 		assertTrue(naslovStraniceWE.getText().trim().equals("KATEGORIJE CIJENA"), "KategorijeCijena: Naziv stranice nije dobar!");
 		assertTrue(brojKolona().size() == 6, "KategorijeCijena: Broj kolona nije dobar! ");
+	}
+	
+	public String dodajKategoriju() {
+		String kategorija = getRandomName();
+		wait.until(ExpectedConditions.elementToBeClickable(dodajBtnWE));
+		dodajBtnWE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(nazivKategorijeWE));
+		nazivKategorijeWE.sendKeys(kategorija);
+		wait.until(ExpectedConditions.elementToBeClickable(dodajKategorijuWE));
+		dodajKategorijuWE.click();
+		return kategorija;
+	}
+	
+	public void verifikujKategoriju(String kategorija) throws InterruptedException {
+		wait.until(ExpectedConditions.elementToBeClickable(filterNazivWE));
+		filterNazivWE.click();
+		filterNazivWE.clear();
+		filterNazivWE.sendKeys(kategorija);
+		filterNazivWE.sendKeys(Keys.ENTER);
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.visibilityOf(jedinstvenElementWE));
+		wait.until(ExpectedConditions.visibilityOf(nazivTabelaWE));
+		wait.until(ExpectedConditions.elementToBeClickable(dodajBtnWE));
+		assertTrue(nazivTabelaWE.getText().equals(kategorija), "KtegorijeCijena: Ime kategorije nije dobro!");
+	}
+	
+	public String izmjeniKategorijuCijene() {
+		String novaKategorija = getRandomName();
+		wait.until(ExpectedConditions.elementToBeClickable(burgerBarWE));
+		burgerBarWE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(urediWE));
+		urediWE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(nazivKategorijeWE));
+		nazivKategorijeWE.sendKeys(novaKategorija);
+		wait.until(ExpectedConditions.elementToBeClickable(dodajKategorijuWE));
+		dodajKategorijuWE.click();
+		return novaKategorija;
 	}
 
 }
---
+++ src/test/java/com/platformX/tests/QA_050_Dodavanje_Kategorije_Cijene_Test.java
@@ -0,0 +1,30 @@
+package com.platformX.tests;
+
+import org.testng.annotations.Test;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import com.platformX.base.BaseTest;
+import com.platformX.page.PocetnaStranica;
+import com.platformX.page.KategorijeCijenaPage;
+import com.platformX.page.LogInPage;
+
+public class QA_050_Dodavanje_Kategorije_Cijene_Test extends BaseTest {
+
+	public QA_050_Dodavanje_Kategorije_Cijene_Test() throws IOException, FileNotFoundException {
+		super();
+	}
+
+	@Test
+	public void qa_047_dodavanje_tarifne_grupe_test() throws Exception {
+		LogInPage logInPage = new LogInPage(driver, PLATFORMX_PROPERTIES);
+		logInPage.verifyLogInPage();
+		logInPage.logIn();
+		PocetnaStranica homePage = new PocetnaStranica(driver);
+		homePage.verifyHomePage();
+		KategorijeCijenaPage kategorijeCijena = homePage.navigateToKategorijeCijena();
+		kategorijeCijena.verifikujKategorijeCijena();
+		String kategorija = kategorijeCijena.dodajKategoriju();
+		kategorijeCijena.verifikujKategoriju(kategorija);
+	}
+
+}
