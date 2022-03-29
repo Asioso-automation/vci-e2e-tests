ae74101715368e17445d02ece7190ae9dcf1c8dd
nebojsa.milojevic <nebojsa.milojevic@qaalpha.com>
2022-03-29 10:43:07 +0200

    tests run

--- src/test/java/com/platformX/base/BaseTest.java
+++ src/test/java/com/platformX/base/BaseTest.java
@@ -61,7 +61,7 @@ public class BaseTest {
 
 	@AfterMethod
 	public void cleanUp() {
- 	driver.quit();
+ 	// driver.quit();
 	endTime = System.currentTimeMillis();
 	duration = endTime - startTime;
 	seconds = (double)duration/1000.0;
