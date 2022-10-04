17a7c2632ca542fb12dd23071bf0ea701395ec67
andjela@dwelt.net <andjela@dwelt.net>
2022-10-04 10:37:02 +0200

    px_dist_006 mala izmjena

--- src/test/java/com/platformX/distribution/tests/PX_DIST_006_Verifikacija_Sekcije_Kupci_Test.java
+++ src/test/java/com/platformX/distribution/tests/PX_DIST_006_Verifikacija_Sekcije_Kupci_Test.java
@@ -1,47 +1,23 @@
 package com.platformX.distribution.tests;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
-import org.testng.annotations.AfterTest;
 import org.testng.annotations.Test;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import com.platformX.base.BaseTest;
 import com.platformX.distribution.page.PocetnaStranica;
 import com.platformX.distribution.page.FizickaLica;
 import com.platformX.distribution.page.LogIn;
 import com.platformX.distribution.page.PravnaLica;
 import com.platformX.distribution.page.ZahtjeviZaDostavuPodatakaKUPCI;
 
-public class PX_DIST_006_Verifikacija_Sekcije_Kupci_Test {
+public class PX_DIST_006_Verifikacija_Sekcije_Kupci_Test extends BaseTest {
 
 	public PX_DIST_006_Verifikacija_Sekcije_Kupci_Test() throws IOException, FileNotFoundException {
 		super();
 	}
 
-	private WebDriver driver;
-	private final String PLATFORMX_PROPERTIES = "platformx.properties";
-
 	@Test(description = "test prolazi kroz sve stranice iz sekcije KUPCI i verifikuje ih")
 	public void px_dist_005_verifikacije_sekcije_kupci_test() throws Exception {
-
-		try {
-			System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-			ChromeOptions options = new ChromeOptions();
-			options.addArguments("--start-maximized");
-			options.addArguments("incognito");
-			options.addArguments("chrome.switches", "--disable-extensions");
-			driver = new ChromeDriver(options);
-		} catch (Exception e) {
-			System.setProperty("webdriver.chrome.driver", "src/test/resources/drivers/chromedriver.exe");
-			ChromeOptions options = new ChromeOptions();
-			options.setBinary("C://Program Files//Google//Chrome//Application//chrome.exe");
-			options.addArguments("--start-maximized");
-			options.addArguments("incognito");
-			options.addArguments("chrome.switches", "--disable-extensions");
-			driver = new ChromeDriver(options);
-		}
-
 		LogIn logIn = new LogIn(driver, PLATFORMX_PROPERTIES);
 		logIn.verifikujLogIn();
 		logIn.logIn();
@@ -53,13 +29,5 @@ public class PX_DIST_006_Verifikacija_Sekcije_Kupci_Test {
 		fizickaLica.verifikujFizickaLica();
 		ZahtjeviZaDostavuPodatakaKUPCI zahtjeviZaDostavuPodatakaKupci = pocetna.navigirajNaZahtjeviZaDostavuPodatakaKUPCI();
 		zahtjeviZaDostavuPodatakaKupci.verifikujZahtjeviZaDostavuPodatakaKUPCI();
-		
-	
 	}
-
-	@AfterTest
-	public void terminateBrowser() {
-		driver.quit();
-	}
-
 }
