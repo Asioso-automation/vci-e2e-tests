0f92f3695ce231f4cfb5960a22b0e1327d3be78e
andjela@dwelt.net <andjela@dwelt.net>
2022-10-12 15:13:32 +0200

    novi test px_dist_dodavanje_fizicke_lokacije_test

--- src/test/java/com/platformX/distribution/page/FizickeLokacije.java
+++ src/test/java/com/platformX/distribution/page/FizickeLokacije.java
@@ -4,9 +4,15 @@ import static org.testng.Assert.assertTrue;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.platformX.base.Kolone;
+import com.platformX.util.Helper;
 
 public class FizickeLokacije extends PocetnaStranica {
 
@@ -14,6 +20,24 @@ public class FizickeLokacije extends PocetnaStranica {
 		super(driver);
 	}
 
+	@FindBy(xpath = "//div[1]/div/div/div/div[1]/div/input")
+	private WebElement nazivWE;
+	
+	@FindBy(xpath = "//div[2]/div[1]/div/div/div[1]/div/input")
+	private WebElement sifraWE;
+	
+	@FindBy(xpath = "//div[2]/div[2]/div/div/div[1]/div[1]/input[1]")
+	private WebElement terenskaJedinicaWE;
+	
+	@FindBy(xpath = "//td[2]/div/div/div/div[1]/input")
+	private WebElement filterPoNazivuWE;
+	
+	@FindBy(xpath = "//tr[2]/td[2]")
+	private WebElement nazivFizickeLokacijeTabelaWE;
+	
+	@FindBy(xpath = "//div[11]/div/div/div/div")
+	private WebElement terenskaJedinicaLookupWE;
+	
 	public void verifikujFizickeLokacije() throws InterruptedException, FileNotFoundException, IOException {
 		Kolone kolone = new Kolone(driver);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
@@ -38,5 +62,45 @@ public class FizickeLokacije extends PocetnaStranica {
 		assertTrue(naslovStraniceWE.getText().trim().equals("FIZIČKE LOKACIJE"), "FizickeLokacije: Naziv stranice nije dobar!");
 		assertTrue(brojKolona().size() == 10, "FizickeLokacije: Broj kolona nije dobar! ");
 	}
-
+	
+	public String dodajFizickuLokaciju(String terenskaJedinica) throws InterruptedException, FileNotFoundException, IOException {
+		String naziv = new String();
+		naziv = "FizickaLokacija " + Helper.getRandomString(5);
+		wait.until(ExpectedConditions.elementToBeClickable(dodajBtnWE));
+		dodajBtnWE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(nazivWE));
+		nazivWE.sendKeys(naziv);
+		wait.until(ExpectedConditions.elementToBeClickable(sifraWE));
+		sifraWE.sendKeys(Helper.getRandomNumber(2));
+		wait.until(ExpectedConditions.elementToBeClickable(terenskaJedinicaWE));
+		terenskaJedinicaWE.sendKeys(terenskaJedinica);
+		wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//div[contains(@class, 'v-list-item__title') and contains(., '" + terenskaJedinica + "')]")));
+		driver.findElement(By.xpath("//div[contains(@class, 'v-list-item__title') and contains(., '" + terenskaJedinica + "')]")).click();
+		wait.until(ExpectedConditions.elementToBeClickable(submitBtnWE));
+		submitBtnWE.click();
+		return naziv;
+	}
+	
+	public void verifikujFizickuLokaciju(String naziv) throws Exception {
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.elementToBeClickable(filterPoNazivuWE));
+		filterPoNazivuWE.click();
+		filterPoNazivuWE.clear();
+		filterPoNazivuWE.sendKeys(naziv);
+		filterPoNazivuWE.sendKeys(Keys.ENTER);
+		wait.until(ExpectedConditions.visibilityOf(nazivFizickeLokacijeTabelaWE));
+		assertTrue(nazivFizickeLokacijeTabelaWE.getText().equals(naziv), "FizickeLokacije: Terenska jedinica nije pronađena!");
+	}
+	
+//	public void verifikujTerenskuJedinicu(String naziv) throws Exception {
+//		WebDriverWait wait = new WebDriverWait(driver, 5);
+//		wait.until(ExpectedConditions.elementToBeClickable(filterPoNazivuWE));
+//		filterPoNazivuWE.click();
+//		filterPoNazivuWE.clear();
+//		filterPoNazivuWE.sendKeys(naziv);
+//		filterPoNazivuWE.sendKeys(Keys.ENTER);
+//		wait.until(ExpectedConditions.visibilityOf(nazivTerenskeJediniceTabelaWE));
+//		assertTrue(nazivTerenskeJediniceTabelaWE.getText().equals(naziv), "TerenskeJedinice: Terenska jedinica nije pronađena!");
+//	}
+	
 }
