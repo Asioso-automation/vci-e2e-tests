799fbe0680293bacbe0b309c53e1a8250b85f661
andjela@dwelt.net <andjela@dwelt.net>
2022-10-14 11:03:46 +0200

    novi test px_dist_dodavanje_citaca_test

--- src/test/java/com/platformX/distribution/page/Citaci.java
+++ src/test/java/com/platformX/distribution/page/Citaci.java
@@ -4,15 +4,36 @@ import static org.testng.Assert.assertTrue;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
 import com.platformX.base.Kolone;
+import com.platformX.util.Helper;
 
 public class Citaci extends PocetnaStranica {
 
 	public Citaci(WebDriver driver) throws FileNotFoundException, IOException {
 		super(driver);
 	}
+	
+	@FindBy(xpath = "//div[1]/div[1]/div/div/div[1]/div/input")
+	private WebElement imeWE;
+	
+	@FindBy(xpath = "(//button[@aria-label = 'prepend icon'])[1]")
+	private WebElement dateIconWE;
+	
+	@FindBy(xpath = "//div[(@class = 'v-btn__content') and contains(text(), '13')]")
+	private WebElement datum13WE;
+	
+	@FindBy(xpath = "//td[2]/div/div/div/div[1]/input")
+	private WebElement filterPoImenuWE;
+	
+	@FindBy(xpath = "//tr[2]/td[2]")
+	private WebElement imeCitacaTabelaWE;
 
 	public void verifikujCitaci() throws InterruptedException, FileNotFoundException, IOException {
 		Kolone kolone = new Kolone(driver);
@@ -37,4 +58,31 @@ public class Citaci extends PocetnaStranica {
 		assertTrue(brojKolona().size() == 8, "Citaci: Broj kolona nije dobar! ");
 	}
 
+	public String dodajCitaca() throws InterruptedException {
+		String ime = new String();
+		ime = "Citac " + Helper.getRandomString(5);
+		wait.until(ExpectedConditions.elementToBeClickable(dodajBtnWE));
+		dodajBtnWE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(imeWE));
+		imeWE.sendKeys(ime);
+		wait.until(ExpectedConditions.elementToBeClickable(dateIconWE));
+		dateIconWE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(datum13WE));
+		datum13WE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(submitBtnWE));
+		submitBtnWE.click();
+		return ime;
+	}
+	
+	public void verifikujCitaca(String ime) throws Exception {
+		WebDriverWait wait = new WebDriverWait(driver, 5);
+		wait.until(ExpectedConditions.elementToBeClickable(filterPoImenuWE));
+		filterPoImenuWE.click();
+		filterPoImenuWE.clear();
+		filterPoImenuWE.sendKeys(ime);
+		filterPoImenuWE.sendKeys(Keys.ENTER);
+		wait.until(ExpectedConditions.visibilityOf(imeCitacaTabelaWE));
+		assertTrue(imeCitacaTabelaWE.getText().equals(ime), "Citaci: Citac nije pronađen!");
+	}
+	
 }
---
+++ src/test/java/com/platformX/distribution/tests/PX_DIST_Dodavanje_Citaca_Test.java
@@ -0,0 +1,31 @@
+package com.platformX.distribution.tests;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.testng.annotations.Test;
+import com.platformX.base.BaseTest;
+import com.platformX.distribution.page.Citaci;
+import com.platformX.distribution.page.LogIn;
+import com.platformX.distribution.page.PocetnaStranica;
+
+
+public class PX_DIST_Dodavanje_Citaca_Test extends BaseTest {
+	
+	public PX_DIST_Dodavanje_Citaca_Test() throws IOException, FileNotFoundException {
+		super();
+	}
+
+	@Test
+	public void px_dist_dodavanje_citaca_test() throws Exception {
+		LogIn logIn = new LogIn(driver, PLATFORMX_PROPERTIES);
+		logIn.verifikujLogIn();
+		logIn.logIn();
+		PocetnaStranica homePage = new PocetnaStranica(driver);
+		homePage.verifikujPocetnuStranicu();
+		Citaci citaci = homePage.navigirajNaCitaci();
+		citaci.verifikujCitaci();
+		String imeCitaca = citaci.dodajCitaca();
+		citaci.verifikujCitaca(imeCitaca);
+	}
+	
+}
