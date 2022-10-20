b517f16a5b0459d105457829e80efffea7a814fb
andjela@dwelt.net <andjela@dwelt.net>
2022-10-20 10:20:57 +0200

    px_006_test en. obracun nova kolona (ID)

--- src/test/java/com/platformX/page/EnergetskiObracun.java
+++ src/test/java/com/platformX/page/EnergetskiObracun.java
@@ -20,6 +20,7 @@ public class EnergetskiObracun extends PocetnaStranica {
 		Kolone kolone = new Kolone(driver);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
 				"//div[contains(@class, 'v-toolbar__title subtitle-2 ml-0 pl-0 text-uppercase') and starts-with(., ' Energetski obračun')]")));
+		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaIdWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaDatumWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaPeriodWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaOdsWE));
@@ -36,7 +37,7 @@ public class EnergetskiObracun extends PocetnaStranica {
 		assertTrue(sekcijaBtnWE.getText().trim().equals("OČITANJA"), "EnergetskiObracun: Naziv sekcije nije dobar!"); // bug, los naziv sekcije
 		assertTrue(stranicaBtnWE.getText().trim().equals("ENERGETSKI OBRAČUN"), "EnergetskiObracun: Naziv stranice nije dobar!");
 		assertTrue(naslovStraniceWE.getText().trim().equals("ENERGETSKI OBRAČUN"), "EnergetskiObracun: Naziv stranice nije dobar!");
-		assertTrue(brojKolona().size() == 9, "EnergetskiObracun: Broj kolona nije dobar! ");
+		assertTrue(brojKolona().size() == 10, "EnergetskiObracun: Broj kolona nije dobar! ");
 	}
 
 }
