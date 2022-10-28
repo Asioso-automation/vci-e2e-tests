36709ae9d66f2d6bb1733fa8f78f110b2cb0b8d9
milan.danilovic@dwelt.net <milan.danilovic@dwelt.net>
2022-10-28 13:23:05 +0200

    novi testovi pxd trafostanice

--- src/test/java/com/platformX/distribution/page/TrafoStanice.java
+++ src/test/java/com/platformX/distribution/page/TrafoStanice.java
@@ -4,15 +4,52 @@ import static org.testng.Assert.assertTrue;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import com.platformX.base.Kolone;
+import com.platformX.util.Helper;
 
 public class TrafoStanice extends PocetnaStranica {
 
 	public TrafoStanice(WebDriver driver) throws FileNotFoundException, IOException {
 		super(driver);
 	}
+	
+	@FindBy(xpath = "//div[2]/div[1]/div/div/div[1]/div/input")
+	private WebElement sifraWE;
+	
+	@FindBy(xpath = "//div[2]/div[2]/div/div/div[1]/div/input")
+	private WebElement nazivWE;
+	
+	@FindBy(xpath = "//div[2]/div[4]/div/div/div[1]/div[1]/input[1]")
+	private WebElement fizickaLokacijaWE;
+	
+	@FindBy(xpath = "//div[2]/div[5]/div/div/div[1]/div/input")
+	private WebElement tipWE;
+	
+	@FindBy(xpath = "//div[3]/div[1]/div/div/div[1]/div[1]/input[1]")
+	private WebElement mjestoWE;
+	
+	@FindBy(xpath = "//div[3]/div[2]/div/div/div[1]/div[1]/input[1]")
+	private WebElement ulicaWE;
+	
+	@FindBy(xpath = "//div[3]/div[4]/div/div/div[1]/div[1]/input[1]")
+	private WebElement postaWE;
+	
+	@FindBy(xpath = "//div[2]/button[1]")
+	private WebElement dodajTrafostanicuWE;
+	
+	@FindBy(xpath = "//td[3]/div/div/div/div[1]/input")
+	private WebElement filterNazivWE;
+	
+	@FindBy(xpath = "//tr[2]/td[3]")
+	private WebElement nazivTabelaWE;
+	
+	@FindBy(xpath = "//td[12]/button")
+	private WebElement burgerBarWE;
 
 	public void verifikujTrafoStanice() throws InterruptedException, FileNotFoundException, IOException {
 		Kolone kolone = new Kolone(driver);
@@ -40,5 +77,77 @@ public class TrafoStanice extends PocetnaStranica {
 		assertTrue(naslovStraniceWE.getText().trim().equals("TRAFOSTANICE"), "TrafoStanice: Naziv stranice nije dobar!");
 		assertTrue(brojKolona().size() == 12, "TrafoStanice: Broj kolona nije dobar! ");
 	}
-
+	
+	public String dodajTrafostanicu()throws Exception{
+		String naziv = "Trafostanica" + Helper.getRandomString(4);
+		wait.until(ExpectedConditions.elementToBeClickable(dodajBtnWE));
+		dodajBtnWE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(sifraWE));
+		sifraWE.click();
+		sifraWE.sendKeys(Helper.getRandomNumber(4));
+		wait.until(ExpectedConditions.elementToBeClickable(nazivWE));
+		nazivWE.click();
+		nazivWE.sendKeys(naziv);
+		wait.until(ExpectedConditions.elementToBeClickable(fizickaLokacijaWE));
+		fizickaLokacijaWE.click();
+		fizickaLokacijaWE.sendKeys(Helper.getRandomNumber(2));
+		Thread.sleep(1000);
+		fizickaLokacijaWE.sendKeys(Keys.ARROW_DOWN);
+		fizickaLokacijaWE.sendKeys(Keys.ENTER);
+		wait.until(ExpectedConditions.elementToBeClickable(tipWE));
+		tipWE.click();
+		tipWE.sendKeys(Helper.getRandomString(4));
+		wait.until(ExpectedConditions.elementToBeClickable(mjestoWE));
+		mjestoWE.click();
+		mjestoWE.sendKeys(Helper.getRandomNumber(2));
+		Thread.sleep(1000);
+		mjestoWE.sendKeys(Keys.ARROW_DOWN);
+		mjestoWE.sendKeys(Keys.ENTER);
+		wait.until(ExpectedConditions.elementToBeClickable(ulicaWE));
+		ulicaWE.click();
+		ulicaWE.sendKeys(Helper.getRandomNumber(2));
+		Thread.sleep(1000);
+		ulicaWE.sendKeys(Keys.ARROW_DOWN);
+		ulicaWE.sendKeys(Keys.ENTER);
+		wait.until(ExpectedConditions.elementToBeClickable(postaWE));
+		postaWE.click();
+		postaWE.sendKeys(Helper.getRandomNumber(2));
+		Thread.sleep(1000);
+		postaWE.sendKeys(Keys.ARROW_DOWN);
+		postaWE.sendKeys(Keys.ENTER);
+		wait.until(ExpectedConditions.elementToBeClickable(dodajTrafostanicuWE));
+		dodajTrafostanicuWE.click();
+		return naziv;
+	}
+	public void verifikujTrafostanicu(String naziv)throws Exception{
+		wait.until(ExpectedConditions.elementToBeClickable(filterNazivWE));
+		Thread.sleep(1000);
+		filterNazivWE.click();
+		filterNazivWE.clear();
+		filterNazivWE.sendKeys(naziv);
+		filterNazivWE.sendKeys(Keys.ENTER);
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.visibilityOf(nazivTabelaWE));
+		assertTrue(nazivTabelaWE.getText().equals(naziv), "TrafoStanice: Trafostanica nije pronadjena!");
+	}
+	public String izmjeniTrafostanicu(String naziv)throws InterruptedException{
+		String trafostanica = "Trafostanica" + Helper.getRandomString(4);
+		wait.until(ExpectedConditions.elementToBeClickable(filterNazivWE));
+		filterNazivWE.click();
+		filterNazivWE.clear();
+		filterNazivWE.sendKeys(naziv);
+		filterNazivWE.sendKeys(Keys.ENTER);
+		wait.until(ExpectedConditions.elementToBeClickable(burgerBarWE));
+		burgerBarWE.click();
+		wait.until(ExpectedConditions.elementToBeClickable(urediWE));
+		urediWE.click();
+		Thread.sleep(1000);
+		wait.until(ExpectedConditions.elementToBeClickable(nazivWE));
+		nazivWE.click();
+		nazivWE.clear();
+		nazivWE.sendKeys(trafostanica);
+		wait.until(ExpectedConditions.elementToBeClickable(dodajTrafostanicuWE));
+		dodajTrafostanicuWE.click();
+		return trafostanica;
+	}
 }
---
+++ src/test/java/com/platformX/distribution/tests/PX_DIST_Dodavanje_Trafostanice_Test.java
@@ -0,0 +1,31 @@
+package com.platformX.distribution.tests;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+import com.platformX.base.BaseTest;
+import com.platformX.distribution.page.LogIn;
+import com.platformX.distribution.page.PocetnaStranica;
+import com.platformX.distribution.page.TrafoStanice;
+
+public class PX_DIST_Dodavanje_Trafostanice_Test extends BaseTest{
+
+	public PX_DIST_Dodavanje_Trafostanice_Test() throws IOException, FileNotFoundException {
+		super();
+	}
+
+	@Test
+	public void px_dist_dodavanje_trafostanice_test() throws Exception {
+		LogIn logIn = new LogIn(driver, PLATFORMX_PROPERTIES);
+		logIn.verifikujLogIn();
+		logIn.logIn();
+		PocetnaStranica homePage = new PocetnaStranica(driver);
+		homePage.verifikujPocetnuStranicu();
+		TrafoStanice trafoStanice = homePage.navigirajNaTrafoStanice();
+		trafoStanice.verifikujTrafoStanice();
+		String trafoStanica = trafoStanice.dodajTrafostanicu();
+		trafoStanice.verifikujTrafostanicu(trafoStanica);
+	}
+}
