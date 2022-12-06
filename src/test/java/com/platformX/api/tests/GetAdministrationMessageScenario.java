dd0777d364c38e5012ca4377f2a955167ea57495
nebojsa.milojevic <nebojsa.milojevic@qaalpha.com>
2022-12-06 20:48:19 +0100

    obrisane zakomentarisane linije u PocetnojStranici px_d dodan RestAssured dependency dodan RestApiBase dodan prvi api test

---
+++ src/test/java/com/platformX/api/tests/GetAdministrationMessageScenario.java
@@ -0,0 +1,21 @@
+package com.platformX.api.tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import com.platformX.base.RestApiBase;
+import io.restassured.response.Response;
+import java.io.IOException;
+
+public class GetAdministrationMessageScenario extends RestApiBase{
+
+	public GetAdministrationMessageScenario() throws IOException {
+		super();
+	}
+
+	@Test
+	public static void get_administration_message_test() {
+		Response response = methodGET("http://10.10.10.21:86/api/Administrations/Messages/Get/1");
+		Assert.assertEquals(response.getStatusCode(), 401, "Correct status code is not returned");
+	}
+	
+}
