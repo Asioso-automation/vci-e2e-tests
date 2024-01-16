95fb1c0bf701124aa2027a25130c2f4b578b04a9
nebojsa.milojevic <nebojsa.milojevic@qaalpha.com>
2024-01-16 21:04:05 +0100

    small follow-up cahanges on API

--- src/test/java/com/platformX/api/tests/PXD_005_BasicCatalogs_FieldUnits_Test.java
+++ src/test/java/com/platformX/api/tests/PXD_005_BasicCatalogs_FieldUnits_Test.java
@@ -20,42 +20,47 @@ public class PXD_005_BasicCatalogs_FieldUnits_Test extends RestApiBase {
 	String token;
 	String id;
 	
+// TODO Sve POST/GET/PUT/DELETE metode iz ove klase ce biti nove metode, koje odmah vracaju JSONPATH,
+// TODO nakon toga refaktorisati testove (linija koja provjerava status ce biti suvisna)
+// TODO Jedino ce u 1. testu "pxd_005_04_create_field_unit_test" biti koristena nova/stara methodPOSTResponse metoda,
+// TODO jer moramo procitati id iz response-a (ne mozemo iz JSONPATH-a)
+
 	@Test(description = "positive test case: create field unit")
-	public void pxd_005_04_create_field_unit_test1() throws Exception {
+	public void pxd_005_04_create_field_unit_test() throws Exception {
 		token = authorize();
 		// Post Field Unit Create
 		addHeader("Authorization", "Bearer " + token);
-		Response response2 = methodPOST("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Create", Payloads.pxdFieldUnitCreate("TerenskaJedinica " + Helper.getRandomString(5)));
-		assertEquals(response2.getStatusCode(), 200);
-		assertNotNull(response2, "Id not forwarded in response");
-		id = response2.asString();
+		Response response = methodPOST("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Create", Payloads.pxdFieldUnitCreate("TerenskaJedinica " + Helper.getRandomString(5)));
+		assertEquals(response.getStatusCode(), 200);
+		assertNotNull(response, "Id not forwarded in response");
+		id = response.asString();
 	}
 
-	@Test(description = "positive test case: get created field unit", dependsOnMethods = { "pxd_005_04_create_field_unit_test1" })
+	@Test(description = "positive test case: get created field unit", dependsOnMethods = { "pxd_005_04_create_field_unit_test" })
 	public void pxd_005_01_get_field_unit_test1() throws Exception {
 		addHeader("Authorization", "Bearer " + token);
-		Response response2 = methodGET("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Get/" + id);
-		assertEquals(response2.getStatusCode(), 200);
-		JsonPath jp2 = new JsonPath(response2.asString());
-		assertNotNull(jp2.getString("id"), "Id not forwarded");
-		assertNotNull(jp2.getString("name"), "Name not forwarded");
-		assertNotNull(jp2.getString("placeText"), "PlaceText not forwarded");
-		assertNotNull(jp2.getString("companyText"), "CompanyText not forwarded");
+		Response response = methodGET("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Get/" + id);
+		assertEquals(response.getStatusCode(), 200);
+		JsonPath jp = new JsonPath(response.asString());
+		assertNotNull(jp.getString("id"), "Id not forwarded");
+		assertNotNull(jp.getString("name"), "Name not forwarded");
+		assertNotNull(jp.getString("placeText"), "PlaceText not forwarded");
+		assertNotNull(jp.getString("companyText"), "CompanyText not forwarded");
 	}
 	
-	@Test(description = "positive test case: update field unit", dependsOnMethods = { "pxd_005_04_create_field_unit_test1" })
-	public void pxd_005_05_update_field_unit_test1() throws Exception {
+	@Test(description = "positive test case: update field unit", dependsOnMethods = { "pxd_005_04_create_field_unit_test" })
+	public void pxd_005_05_update_field_unit_test() throws Exception {
 		addHeader("Authorization", "Bearer " + token);
-		Response response2 = methodPUT("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Update/" + id, Payloads.pxdFieldUnitUpdate(PageBase.id1, "TerenskaJedinica 1", " - "," - "));
-		assertEquals(response2.getStatusCode(), 204);
+		Response response = methodPUT("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Update/" + id, Payloads.pxdFieldUnitUpdate(PageBase.id1, "TerenskaJedinica 1", " - "," - "));
+		assertEquals(response.getStatusCode(), 204);
 	}
 	
-	@Test(description = "positive test case: delete field unit", dependsOnMethods = { "pxd_005_04_create_field_unit_test1" })
-	public void pxd_005_06_delete_field_unit_test1() throws Exception {
+	@Test(description = "positive test case: delete field unit", dependsOnMethods = { "pxd_005_04_create_field_unit_test" })
+	public void pxd_005_06_delete_field_unit_test() throws Exception {
 		addHeader("Authorization", "Bearer " + token);
-		Response response2 = methodDELETE("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Delete/" + id);
-		assertEquals(response2.getStatusCode(), 204);
-		assertEquals(response2.print(), "");
+		Response response = methodDELETE("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Delete/" + id);
+		assertEquals(response.getStatusCode(), 204);
+		assertEquals(response.print(), "");
 	}
 	
 	@Test(description = "negative test case: bearer token missing")
@@ -72,40 +77,40 @@ public class PXD_005_BasicCatalogs_FieldUnits_Test extends RestApiBase {
 		assertEquals(response.getStatusCode(), 401);
 	}
 		
-	@Test(description = "negative test case: wrong id", dependsOnMethods = { "pxd_005_04_create_field_unit_test1" })
+	@Test(description = "negative test case: wrong id", dependsOnMethods = { "pxd_005_04_create_field_unit_test" })
 	public void pxd_005_01_get_field_unit_test4() throws Exception {
 		addHeader("Authorization", "Bearer " + token);
-		Response response2 = methodGET("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Get/" + Helper.getRandomNumber(6));
-		assertEquals(response2.getStatusCode(), 404);
+		Response response = methodGET("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Get/" + Helper.getRandomNumber(6));
+		assertEquals(response.getStatusCode(), 404);
 	}
 	
-	@Test(description = "positive test case: list", dependsOnMethods = { "pxd_005_04_create_field_unit_test1" })
-	public void pxd_005_02_post_field_units_list_test1() throws Exception {
+	@Test(description = "positive test case: list", dependsOnMethods = { "pxd_005_04_create_field_unit_test" })
+	public void pxd_005_02_post_field_units_list_test() throws Exception {
 		addHeader("Authorization", "Bearer " + token);
-		Response response2 = methodPOST("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/List", Payloads.pxdBasicList(0, 10, "id", "DESC"));
-		assertEquals(response2.getStatusCode(), 200);
-		JsonPath jp2 = new JsonPath(response2.asString());
-		assertNotNull(jp2.getString("totalCount"), "TotalCount not forwarded");
-		assertNotNull(jp2.getString("filteredCount"), "FilteredCount not forwarded");
-		assertNotNull(jp2.getString("dataCount"), "DataCount not forwarded");
-		assertNotNull(jp2.getString("data"), "Data not forwarded");
+		Response response = methodPOST("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/List", Payloads.pxdBasicList(0, 10, "id", "DESC"));
+		assertEquals(response.getStatusCode(), 200);
+		JsonPath jp = new JsonPath(response.asString());
+		assertNotNull(jp.getString("totalCount"), "TotalCount not forwarded");
+		assertNotNull(jp.getString("filteredCount"), "FilteredCount not forwarded");
+		assertNotNull(jp.getString("dataCount"), "DataCount not forwarded");
+		assertNotNull(jp.getString("data"), "Data not forwarded");
 	}
 	
-	@Test(description = "positive test case", dependsOnMethods = { "pxd_005_04_create_field_unit_test1" })
+	@Test(description = "positive test case: lookup", dependsOnMethods = { "pxd_005_04_create_field_unit_test" })
 	public void pxd_005_03_get_field_unit_lookup_test1() throws Exception {
 		addHeader("Authorization", "Bearer " + token);
-		Response response2 = methodGET("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Lookup?Keyword=" + id + "&Id=" + id);
-		assertEquals(response2.getStatusCode(), 200);
-		JsonPath jp2 = new JsonPath(response2.asString());
-		assertNotNull(jp2.getString("id"), "Id not forwarded");
-		assertNotNull(jp2.getString("text"), "Text not forwarded");
+		Response response = methodGET("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Lookup?Keyword=" + id + "&Id=" + id);
+		assertEquals(response.getStatusCode(), 200);
+		JsonPath jp = new JsonPath(response.asString());
+		assertNotNull(jp.getString("id"), "Id not forwarded");
+		assertNotNull(jp.getString("text"), "Text not forwarded");
 	}
 	
-	@Test(description = "negative test case: wrong id", dependsOnMethods = { "pxd_005_04_create_field_unit_test1" })
+	@Test(description = "negative test case: wrong id", dependsOnMethods = { "pxd_005_04_create_field_unit_test" })
 	public void pxd_005_03_get_field_unit_lookup_test2() throws Exception {
 		addHeader("Authorization", "Bearer " + token);
-		Response response2 = methodGET("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Lookup?Keyword=" + Helper.getRandomNumber(6) + "&Id=" + Helper.getRandomNumber(6));
-		assertEquals(response2.getStatusCode(), 200);
+		Response response = methodGET("http://10.10.10.21:8086/api/BasicCatalogs/FieldUnits/Lookup?Keyword=" + Helper.getRandomNumber(6) + "&Id=" + Helper.getRandomNumber(6));
+		assertEquals(response.getStatusCode(), 200);
 	}
 	
 }
