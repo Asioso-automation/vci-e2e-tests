b7778070ca281efc24f6fe1835bd2c7fd7b62f56
andjela <andjela@dwelt.net>
2024-06-05 17:15:57 +0200

    PXD: 2 nove kolone

--- src/test/java/com/platformX/distribution/page/ImportiOcitanja.java
+++ src/test/java/com/platformX/distribution/page/ImportiOcitanja.java
@@ -17,11 +17,12 @@ public class ImportiOcitanja extends PocetnaStranicaPXD {
 		Kolone kolone = new Kolone(driver);
 		wait.until(ExpectedConditions.visibilityOfElementLocated(By.xpath(
 				"//div[contains(@class, 'v-toolbar__title subtitle-2 ml-0 pl-0 text-default') and starts-with(., ' Importi očitanja')]")));
-		verifikacijaZajednickihElemenata("Očitanja", "Importi Očitanja", "Importi očitanja", 15, true, false, false, true, true, true, false);
+		verifikacijaZajednickihElemenata("Očitanja", "Importi Očitanja", "Importi očitanja", 16, true, false, false, true, true, true, false);
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaIdWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaKorisnikWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaCitacWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaDatumWE));
+		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaCitackiHodWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaTrafostanicaWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaFizickaLokacijaWE));
 		wait.until(ExpectedConditions.visibilityOf(kolone.kolonaDatumPrvogOcitanjaWE));
